395da73d6c30b3016b3cd5e4f33a77cfb2914c1f
+ <+>
2023-08-14 04:54:35 -0400

    looping answer

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i= 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+  
+  return gifts; 
+
+}
+
+wrapGifts(gifts);
+
+function writeCards(namesArray, eventName) {
+  let messagesArray= [];
+
+  for (let i=0; i < namesArray.length; i++){
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+    messagesArray.push(message);
+  }
+   return messagesArray;
+}
+
+const names = ["Alice", "Bob", "Carol"];
+const event = "birthday";
+
+const messages = writeCards(names,event);
+console.log(messages);
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
+
